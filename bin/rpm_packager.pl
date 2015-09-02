eb0b846d3d56f5e9c5c59afc6172b70b5d5271e8
satoshi <satoshi.yagi@yahoo.com>
2015-09-02 12:21:01 -0700

    enhancing the wrapper script a bit

--- bin/rpm_packager.pl
+++ bin/rpm_packager.pl
@@ -5,6 +5,21 @@ use warnings;
 use YAML;
 use RPM::Packager;
 
-my $config   = YAML::LoadFile( $ARGV[0] );
+my $arg = shift;
+usage() unless ( $arg && -f $arg );
+
+my $config = YAML::LoadFile($arg);
+my @rpms   = glob "*.rpm";
+unlink @rpms;
+
 my $packager = RPM::Packager->new( %{$config} );
 $packager->create_rpm();
+
+sub usage {
+    print <<USAGE;
+Create RPM from YAML manifest
+
+    $0 <path_to_YAML>
+USAGE
+    exit(-1);
+}
